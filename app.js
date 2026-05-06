a3810c1116a8114d22fa3adf712d1822ff1fcfb1
PiyushXmishra <piyushbhp83@gmail.com>
2026-05-06 08:40:16 +0530

    Base Commit

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var path = require("path");
+
 var app = express();
-var bodyParser = require('body-parser');
-var passport=require("passport");
-var methodOverride= require("method-override");
-var path = require('path');
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname));
 app.use(methodOverride("_method"));
 
-app.get("/", function(req, res){
-	res.render("index");
-});
-app.get("/login", function(req, res){
-	res.render("login");
-});
-app.get("/signup", function(req, res){
-		res.render("signup");
-});
-app.post("/signup", function(req, res){
-	res.redirect("/maps");
-});
-app.post("/login", function(req, res){
-	res.redirect("/maps");
-});
-app.get("/maps", function(req, res){
-	res.sendFile(path.join(__dirname + '/MapAndLoc.html'));
+app.get("/", function(req, res) {
+    res.render("index");
 });
 
-app.listen(3000, function(){
-    console.log("Safe Path Recommender Ready to use in Browser ~(^_^)~");
+app.get("/login", function(req, res) {
+    res.render("login");
+});
 
+app.get("/signup", function(req, res) {
+    res.render("signup");
+});
+
+app.post("/signup", function(req, res) {
+    res.redirect("/maps");
+});
+
+app.post("/login", function(req, res) {
+    res.redirect("/maps");
+});
+
+app.get("/maps", function(req, res) {
+    res.sendFile(path.join(__dirname, "MapAndLoc.html"));
+});
+
+app.listen(3000, function() {
+    console.log("Safe Path Recommender is running on http://localhost:3000");
 });
--- mocha.js
+++ mocha.js
@@ -1,48 +1,55 @@
-const mongoose = require('mongoose');
-mongoose.Promise =  global.Promise; 
+const mongoose = require("mongoose");
+const assert = require("assert");
 
-before(() => {
-    mongoose.connect('mongodb://192.168.100.100:27020');
+mongoose.Promise = global.Promise;
+
+before(function(done) {
+    mongoose.connect("mongodb://192.168.100.100:27020");
     mongoose.connection
-    .once('open', () => {}))
-    .on('error', (error) => {
-    console.warn('Warning', error);
-    });
-});
-//User.js
-const Schema = mongoose.Schema;
-const UserSchema = new Schema({
-    npm_pol: String,
-    lati: float,
-    long: float,
-    mag: integer
-});
-const User = mongoose.model('user', UserSchema);
-module.exports = User;
-
-
-//Create Tests
-const assert = require('assert');
-describe('Reading Crime Records out of the database', () => {
-    beforeEach(() => {
-        const lati = new User({name: 'lati'});
-        const long = new User({name: 'long'});
-        lati.save();
-        long.save();
-        .then(() => {
+        .once("open", function() {
             done();
+        })
+        .on("error", function(error) {
+            console.warn("Warning", error);
+        });
+});
+
+const Schema = mongoose.Schema;
+const CrimeSchema = new Schema({
+    nm_pol: String,
+    lati: Number,
+    long: Number,
+    mag: Number
+});
+
+const CrimeRecord = mongoose.model("crime_record", CrimeSchema);
+
+describe("Reading crime records from the database", function() {
+    var savedRecord;
+
+    beforeEach(function(done) {
+        savedRecord = new CrimeRecord({
+            nm_pol: "Sample Area",
+            lati: 28.6139,
+            long: 77.2090,
+            mag: 2
+        });
+
+        savedRecord.save().then(function() {
+            done();
+        });
     });
-    it('finds the tuple with the name of city', (done) => {
-        User.findOne({nm_pol: city})
-        .then((users) => {
-        assert(users[i]._id.toString() === data._id.toString());
-        done();
+
+    it("finds a record by police area", function(done) {
+        CrimeRecord.findOne({ nm_pol: "Sample Area" }).then(function(record) {
+            assert(record._id.toString() === savedRecord._id.toString());
+            done();
         });
     });
 });
 
-beforeEach((done) => {
-    mongoose.connection.collections.users.drop(() => {
+afterEach(function(done) {
+    mongoose.connection.collections.crime_records.drop(function() {
         done();
     });
-})
+});
